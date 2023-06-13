a8f9c1887f81eddb114025f69514edf6fb53eb42
Catherine <66266208+CatherineKiiru@users.noreply.github.com>
2023-06-13 21:02:07 +0300

    Created a delete button function that places the trach icon at the end of every new urls generated

--- app.js
+++ app.js
@@ -10,71 +10,81 @@ const alert = document.querySelector(".alert");
 // set the main url container's class to url-container and id to "${id}".
 // Create divs for the old url & the new url
 //create another div with "options" class to hold 2 buttons:
-// One button is for copying the new url and another button is for deleting the copied url 
-// In the second button, insert a trash can icon to symbolize the delete button 
+// One button is for copying the new url and another button is for deleting the copied url
+// In the second button, insert a trash can icon to symbolize the delete button
 
-function shortURLOutput(id, oldURL, newURL) {
+function shortURLOutput(id, oldUrl, newUrl) {
   urlWrapper.insertAdjacentHTML(
     "beforeend",
-    <div class="url-container" id="${id}">
-    <div class="old-url">
-      <p><a href="${oldURL}" target="_blank">${oldURL}</a></p>
-    </div>
-    <div class="new-url">
-      <p><a href="${newURL}" target="_blank">${newURL}</a></p>
-    </div>
-    <div class="options">
-      <button type="button" class="copy-new-url btn btn-sm scale-effect">copy</button>
+    <div class="url-shorten-results" id="${id}">
+      <div class="old-url">
+        <p>
+          <a href="${oldUrl}" target="_blank">
+            ${oldUrl}
+          </a>
+        </p>
+      </div>
+      <div class="new-url">
+        <p>
+          <a href="${newURL}" target="_blank">
+            ${newUrl}
+          </a>
+        </p>
+      </div>
+      <div class="options">
+        <button type="button" class="copy-new-url btn btn-sm scale-effect">
+          copy
+        </button>
 
-      <button type="button" class="delete-url scale-effect">
-        <i class="fa-regular fa-trash-can icon"></i>
-      </button>
+        <button type="button" class="delete-url scale-effect">
+          <i class="fa-regular fa-trash-can icon"></i>
+        </button>
+      </div>
     </div>
-  </div>
   );
-  // These are functions to manipulate the URL behavior. The first function removes the URL, 
+  // These are functions to manipulate the URL behavior. The first function removes the URL,
   //The second URL copies the new URL and the removeAllURLs deletes the copies URLs
-  removeURL();
-  copyURL();
-  removeAllURLs(); 
-};
+  removeUrl();
+  copyUrl();
+  removeAllUrls();
+}
 
 // Place delete button at the end of generated links function
 
-function deleteButton(newNode, existingNode) {
-  existingNode.parentNode.insertBefore(newNode, existingNode, nextSibling);
-}
+deleteButton = (newNode, currentNode) => {
+  currentNode.parentNode.insertBefore(
+    newNode,
+    currentNode,
+    currentNode.nextSibling
+  );
+};
 
-// Function for removing all generated URLs
-
-function deleteGeneratedURLs () {
-  if (urlWrapper.querySelectorAll(".url-container").length >=2) {
-    if (urlWrapper.querySelector("delete-all-urls")) {
-      urlWrapper.querySelector("delete-all-urls").remove();
+removeAllNewURLs = () => {
+  // Checks whether there are two url-shorten-results classes in the urlWrapper element.
+  // If the condition is true, this function creates a "delete all button" to remove two generated URLs  
+  if (urlWrapper.querySelectorAll(".url-shorten-results").length >= 2) {
+    if (urlWrapper.querySelector(".delete-all-urls")) {
+      urlWrapper.querySelector(".delete-all-urls").remove();
     }
-    // Create delete generated URLS button 
+    // we're creating a button with the "delete-all" text
+    // you first define a variable with the createElement method
+    //then 
     let button = document.createElement("button");
-    button.type = "button"; 
+    button.type = "button";
     button.classList = "btn btn-sm delete-all-urls scale-effect";
     button.textContent = "delete all";
     insertAfter(button, urlWrapper.lastElementChild);
 
-    //delete generated URLS and Local storage
-    let deleteAll = urlWrapper.querySelector(".delete-all-urls");
-    deleteAll.addEventListener("click", () => {
+    let deleteAllUrls = urlWrapper.querySelector(".delete-all-urls");
+    deleteAllUrls.addEventListener("click", () => {
       urlWrapper.innerHTML = "";
-      savedURLS = [];
+      savedUrls = [];
       localStorage.removeItem("saved");
     });
   } else {
-    if (urlWrapper.querySelector(".delete-all-urls")) {
-      urlWrapper.querySelector("delete-all-urls").remove();
+    if (urlWrapper.querySelector("delete-all-urls")) {
+      urlWrapper.querySelector(".delete-all-urls").remove();
     }
   }
-}
-
-// function for deleting one userSelect: 
-
-function removeOneURL (){
-  let 
-}
+  // Create dynamic button for deleting URLs
+};
